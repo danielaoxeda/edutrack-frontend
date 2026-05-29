682949196d53f50164dc77137c743a0132c94b03
Dieg0Santos1 <alexanderasa0210@gmail.com>
2026-05-28 22:42:51 -0500

    feat(teacher): integrate all new teacher page routes in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,13 @@ import {
 import DashboardEstudiantePage from "./features/student/pages/StudentDashboard";
 import ActivitiesPage from "./features/student/pages/ActivitiesPage";
 import SettingsPage from "./features/student/pages/SettingsPage.tsx";
+import TeacherDashboardPage from "./features/teacher/pages/TeacherDashboard";
+import TeacherCoursesPage from "./features/teacher/pages/TeacherCourses";
+import TeacherStudentsPage from "./features/teacher/pages/TeacherStudents";
+import TeacherTasksPage from "./features/teacher/pages/TeacherTasks";
+import TeacherGradesPage from "./features/teacher/pages/TeacherGrades";
+import TeacherAttendancePage from "./features/teacher/pages/TeacherAttendance";
+import TeacherMessagesPage from "./features/teacher/pages/TeacherMessages";
 import AuthPage from "./pages/AuthPage";
 
 function App() {
@@ -21,7 +28,7 @@ function App() {
                     element={<AuthPage />}
                 />
 
-                {/* DASHBOARD */}
+                {/* DASHBOARD ESTUDIANTE */}
                 <Route
                     path="/dashboard-estudiante"
                     element={<DashboardEstudiantePage />}
@@ -37,6 +44,47 @@ function App() {
                     element={<SettingsPage />}
                 />
 
+                {/* DASHBOARD DOCENTE */}
+                <Route
+                    path="/dashboard-docente"
+                    element={<TeacherDashboardPage />}
+                />
+
+                {/* CURSOS DOCENTE */}
+                <Route
+                    path="/cursos-docente"
+                    element={<TeacherCoursesPage />}
+                />
+
+                {/* ESTUDIANTES DOCENTE */}
+                <Route
+                    path="/estudiantes-docente"
+                    element={<TeacherStudentsPage />}
+                />
+
+                {/* TAREAS DOCENTE */}
+                <Route
+                    path="/tareas-docente"
+                    element={<TeacherTasksPage />}
+                />
+
+                {/* CALIFICACIONES DOCENTE */}
+                <Route
+                    path="/calificaciones-docente"
+                    element={<TeacherGradesPage />}
+                />
+
+                {/* ASISTENCIA DOCENTE */}
+                <Route
+                    path="/asistencia-docente"
+                    element={<TeacherAttendancePage />}
+                />
+
+                {/* MENSAJES DOCENTE */}
+                <Route
+                    path="/mensajes-docente"
+                    element={<TeacherMessagesPage />}
+                />
 
                 {/* REDIRECT */}
                 <Route
